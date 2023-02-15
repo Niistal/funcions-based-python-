f0a8d3c4b4cd7fa2b3f7b59d12b7cd5cdc807bb0
de la Torre Barros <delatorre.aiert@uni.lan>
2023-02-15 14:05:12 +0100

    finished nistal

--- Paper.py
+++ Paper.py
@@ -379,6 +379,245 @@ def Preguntadosbutrandomized():
                 count + 1
     print("You answered" + score + "correctly")
 
+    def history(score,num):
+    qhist = ["¿Who discovered America?", " ¿In which year finished World War 2", "¿In which country was born Hitler?"]
+    ahist = ["Cristobal Colon", "1945", "Austria"]
+
+    a = random.randrange(0, 3)
+
+    if a == 1:
+        preg = "¿Who discovered America?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "cristobal colon":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 2:
+        preg = "¿In which year finished World War 2"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "1945":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 3:
+        preg = "¿In which country was born Hitler?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "austria":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+
+def sport(score,num):
+    qspor = ["¿which country won the world cup in 2022? ", "¿who is the the player who scored more points in the NBA?",
+             "¿Who is the fastest athlete in the history?"]
+    aspor = ["Argentina", "Lebron James", "Usain Bolt"]
+
+    a = random.randrange(0, 3)
+    if a == 1:
+        preg = "¿which country won the world cup in 2022? "
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "argentina":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 2:
+        preg = "¿who is the the player who scored more points in the NBA?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "lebron james":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 3:
+        preg = "¿Who is the fastest athlete in the history?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "usain bolt":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+
+def entertainment(score,num):
+    qentertainment = ["Who is the main character in the pokémon series?",
+                      "What is the most famous technique of goku from dragon ball?",
+                      "What is the name of the film that toys have sense of life?"]
+    aentertainment = ["Ash Ketchum", "Kamehameha", "Toy Story"]
+
+    a = random.randrange(0, 3)
+    if a == 1:
+        preg = "Who is the main character in the pokémon series?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower()== "ash ketchum":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 2:
+        preg = "What is the most famous technique of goku from dragon ball?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "kamehameha":
+            print("the answer is Correct")
+            score + 1
+
+        else:
+            print("the answer is incorrect")
+
+    if a == 3:
+        preg = "What is the name of the film that toys have sense of life?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "toy story":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+
+def geography(score,num):
+    qgeo = ["¿which is the country with the highest population?", "¿which is the highest mountain in the world?",
+            "which is the capital of the USA "]
+    ageo = ["China", "Everest", "Washington DC"]
+
+    a = random.randrange(0, 3)
+    if a == 1:
+        preg = "¿which is the country with the highest population?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "china":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 2:
+        preg = "¿which is the highest mountain in the world?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "everest":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 3:
+        preg = "which is the capital of the USA "
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "washington dc":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+
+def art(score,num):
+    qart = ["Who painted the Mona Lisa?", "Who painted the last dinner?",
+            "What city is depicted in Van Gogh's The Starry Night?"]
+    aart = ["Leonardo da Vinci", "Leonardo da Vinci", "Vincent van Gogh"]
+    a = randint(0, 3)
+
+    if a == 1:
+        preg = "Who painted the Mona Lisa?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "leonardo da vinci":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 2:
+        preg = "Who painted the last dinner?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "leonardo da vinci":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+    if a == 3:
+        preg = "What city is depicted in Van Gogh's The Starry Night?"
+        print(preg)
+        erantzuna = input()
+
+        if erantzuna.lower() == "vincent van gogh":
+            print("the answer is Correct")
+            score + 1
+        else:
+            print("the answer is incorrect")
+
+
+def NistalMain():
+
+    score = 0
+    name = input("Enter your name please ")
+    num=0
+    while num != 6:
+
+        print("Hello " + name + " you are gonna play a quiz and you have a few topics and to win you have to answer the questions as fast as you can\n")
+
+        print("TOPICS")
+        print("___________________")
+        print("1-History")
+        print("2-Sport")
+        print("3-Entertainment")
+        print("4-Geography")
+        print("5-Art")
+        print("6-exit")
+        print("___________________")
+        num = int(input("what topic do you want \n"))
+
+        if num == 1:
+            history(score,num)
+
+        elif num == 2:
+            sport(score,num)
+
+        elif num == 3:
+            entertainment(score,num)
+
+        elif num == 4:
+            geography(score,num)
+
+        elif num == 5:
+            art(score,num)
+
+    print(score)
+
 a = 0
 #Menu
 print("MENU:")
@@ -398,7 +637,7 @@ match a:
         if q == 1:
             Preguntadosbutrandomized()
         elif q == 2:
-            #normalpreguntados()
+            NistalMain()
             pass
         else:
             print("Not a valid number")
